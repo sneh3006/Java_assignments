72f199168f1fda60bd8a83f1819724be0cbbf801
Sneh Jani <snehmegh3006@gmail.com>
2022-09-26 13:26:43 +0530

    This is second commit from vs code

--- Java assingnment-3/tryException2.java
+++ Java assingnment-3/tryException2.java
@@ -1,9 +1,23 @@
-public class ArgsTest{
+class tryException2 {
 
-  public static void main(String[] args) {
+    public static void main(String[] args){
+      int a;
+      int[] arr = new int[10];
+      String str = null;
   
-  System.out.println(args[0] + " " + args[args.length-1]);
   
-  }
-  
-  }  
+      try {
+        a = 10 / 0;
+        arr[15] = 16;
+      } catch (ArithmeticException e) {
+        System.out.println("This is Arithmetic Exeption");
+      }
+      catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("Array Out Of Bound exception");
+      }
+      finally
+      {
+        System.out.println("This is Final Statement");
+      }
+    }
+}
